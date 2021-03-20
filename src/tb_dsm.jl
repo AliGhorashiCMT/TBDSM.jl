27e8a15cb35888e2580b1ac8e7c9188ce371181a
Ali Ghorashi <aligho@mit.edu>
2021-03-20 18:05:54 -0400

    graphene im_pol method

--- src/tb_dsm.jl
+++ src/tb_dsm.jl
@@ -1,19 +1,21 @@
 module tb_dsm
 #Dependencies
-using PyCall, PyPlot
+using PyCall, PyPlot, LinearAlgebra
 
 const pb = PyNULL()
 const pb_repo = PyNULL()
 const pb_graphene = PyNULL()
+const pb_mos2 = PyNULL()
 const pb_model = PyNULL()
 const pb_solver = PyNULL()
 const pb_lattice = PyNULL()
-export pb, pb_repo, pb_graphene, pb_model, pb_solver, pb_lattice
+export pb, pb_repo, pb_graphene, pb_model, pb_solver, pb_lattice, pb_mos2
 
 function __init__()
     copy!(pb, pyimport("pybinding"))
     copy!(pb_repo, pyimport("pybinding.repository"))
     copy!(pb_graphene, pyimport("pybinding.repository.graphene"))
+    copy!(pb_mos2, pyimport("pybinding.repository.group6_tmd").monolayer_3band("MoS2"))
     copy!(pb_model, pb.Model)
     copy!(pb_solver, pb.solver.lapack)
     copy!(pb_lattice, pb.Lattice)
@@ -26,6 +28,6 @@ include("tb_2d_dielectric.jl")
 include("tb_3d_dielectric.jl")
 
 include("tb_specific_examples.jl")
-export graphene_bands
+export graphene_bands, bilayer_graphene_bands, tmd_mo_s2, graphene_impol
 
 end # module
--- src/tb_specific_examples.jl
+++ src/tb_specific_examples.jl
@@ -4,8 +4,49 @@ function graphene_bands()
     K2 = [2*pi / (3*sqrt(3)*a_cc), 2*pi / (3*a_cc)]
     M = [0, 2*pi / (3*a_cc)]
     K1 = [-4*pi / (3*sqrt(3)*a_cc), 0]
-    graphene_mod = model(pb_graphene.monolayer(), pb.translational_symmetry())
-    solver(graphene_mod).calc_bands(K1, Gamma, M, K2).plot()
+    graphene_mod = pb_model(pb_graphene.monolayer(), pb.translational_symmetry())
+    pb_solver(graphene_mod).calc_bands(K1, Gamma, M, K2).plot()
+end
+
+function heaviside(x::Real)
+    x>0 ? 1 : 0
+end
+
+function graphene_impol(qx::Real, qy::Real, μ::Real;mesh::Int=10, histogram_width::Real=100)
+
+    im_pols = zeros(histogram_width*30)
+    b1, b2 = pb_graphene.monolayer().reciprocal_vectors()
+    bzone_area = abs(cross(b1, b2)[3])
+    graphene_mod = pb_model(pb_graphene.monolayer(), pb.translational_symmetry())
+    graphene_solver = pb_solver(graphene_mod)
+    for xiter in 1:mesh
+        for yiter in 1:mesh
+            kx, ky = xiter/mesh*b1[1] + yiter/mesh*b2[1], xiter/mesh*b1[2] + yiter/mesh*b2[2]
+            graphene_solver.set_wave_vector([kx, ky])
+            Ednk, Eupk = graphene_solver.eigenvalues[1], graphene_solver.eigenvalues[2]
+            vecdnk, vecupk = graphene_solver.eigenvectors[:, 1], graphene_solver.eigenvectors[:, 2]
+
+            graphene_solver.set_wave_vector([kx, ky]+[qx, qy])
+            Ednkpluq, Eupkplusq = graphene_solver.eigenvalues[1], graphene_solver.eigenvalues[2]
+
+            vecdnkplusq, vecupkplusq = graphene_solver.eigenvectors[:, 1], graphene_solver.eigenvectors[:, 2]
+
+            overlap1 = abs(sum(conj(vecupkplusq).*vecdnk))^2
+            overlap2 = abs(sum(conj(vecupkplusq).*vecupk))^2
+
+            ω = Eupkplusq - Ednk
+            f2 = heaviside(μ-Eupkplusq)
+            f1 = heaviside(μ-Ednk)
+            im_pols[round(Int, ω*histogram_width+1)] = im_pols[round(Int, ω*histogram_width+1)] + 2/(2π)^2*π*histogram_width*(f2-f1)*overlap1*(1/mesh)^2*bzone_area
+
+            ω = Eupkplusq - Eupk
+            f1 = heaviside(μ-Eupk)
+            if ω > 0
+                im_pols[round(Int, ω*histogram_width+1)] = im_pols[round(Int, ω*histogram_width+1)] + 2/(2π)^2*π*histogram_width*(f2-f1)*overlap2*(1/mesh)^2*bzone_area
+            end            
+        end
+    end
+    return im_pols
 end
 
 function bilayer_graphene_bands()
@@ -14,6 +55,12 @@ function bilayer_graphene_bands()
     K2 = [2*pi / (3*sqrt(3)*a_cc), 2*pi / (3*a_cc)]
     M = [0, 2*pi / (3*a_cc)]
     K1 = [-4*pi / (3*sqrt(3)*a_cc), 0]
-    graphene_mod = model(pb_graphene.bilayer(), pb.translational_symmetry())
-    solver(graphene_mod).calc_bands(K1, Gamma, M, K2).plot()
+    graphene_mod = pb_model(pb_graphene.bilayer(), pb.translational_symmetry())
+    pb_solver(graphene_mod).calc_bands(K1, Gamma, M, K2).plot()
+end
+
+function tmd_mo_s2()
+    mos2_mod = pb_model(pb_mos2, pb.translational_symmetry())
+    kpoints1, kpoints2, kpoints3 = [0, 0], mos2_mod.lattice.brillouin_zone()[1], mos2_mod.lattice.brillouin_zone()[2]
+    pb_solver(mos2_mod).calc_bands(kpoints1, kpoints2, kpoints3).plot()
 end
